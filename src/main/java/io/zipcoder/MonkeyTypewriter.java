0fb89bf1d75794a9057c00aafdf45642308d5fe6
Matt Kramer <matthew123003@gmail.com>
2024-04-18 19:20:37 -0400

    Finished

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -23,33 +23,59 @@ public class MonkeyTypewriter {
         // Do all of the Monkey / Thread building here
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
-            SafeCopier monkey1 = new SafeCopier(introduction);
-            SafeCopier monkey2 = new SafeCopier(introduction);
-            SafeCopier monkey3 = new SafeCopier(introduction);
-            SafeCopier monkey4 = new SafeCopier(introduction);
-            SafeCopier monkey5 = new SafeCopier(introduction);
-            UnsafeCopier monkey6 = new UnsafeCopier(introduction);
-            UnsafeCopier monkey7 = new UnsafeCopier(introduction);
-            UnsafeCopier monkey8 = new UnsafeCopier(introduction);
-            UnsafeCopier monkey9 = new UnsafeCopier(introduction);
-            UnsafeCopier monkey10 = new UnsafeCopier(introduction);
+            UnsafeCopier unsafe1 = new UnsafeCopier(introduction);
+            Thread monkey1 = new Thread(unsafe1, "monkey1");
+            Thread monkey2 = new Thread(unsafe1, "monkey2");
+            Thread monkey3 = new Thread(unsafe1, "monkey3");
+            Thread monkey4 = new Thread(unsafe1, "monkey4");
+            Thread monkey5 = new Thread(unsafe1, "monkey5");
 
-            monkey1.run();
-            monkey2.run();
-            monkey3.run();
-            monkey4.run();
-            monkey5.run();
-            monkey6.run();
-            monkey7.run();
-            monkey8.run();
-            monkey9.run();
-            monkey10.run();
+            //These monkeys threw exceptions, saw concurrency issues
+            monkey1.start();
+            monkey2.start();
+            monkey3.start();
+            monkey4.start();
+            monkey5.start();
+
+           try {
+               monkey1.join();
+               monkey2.join();
+               monkey3.join();
+               monkey4.join();
+               monkey5.join();
+           }catch(InterruptedException e){
+               e.printStackTrace();
+               System.out.println("Unsafe Monkeys Interrupted");
+           }
+
+           SafeCopier safe1 = new SafeCopier(introduction);
+           Thread monkey6 = new Thread(safe1, "monkey6");
+           Thread monkey7 = new Thread(safe1, "monkey7");
+           Thread monkey8 = new Thread(safe1, "monkey8");
+           Thread monkey9 = new Thread(safe1, "monkey9");
+           Thread monkey10 = new Thread(safe1, "monkey10");
+
+           //These monkeys everything went great, because of the lock.
+           //Allows for synchronous threads going over the same file or resource
+           monkey6.start();
+           monkey7.start();
+           monkey8.start();
+           monkey9.start();
+           monkey10.start();
+           try {
+               monkey6.join();
+               monkey7.join();
+               monkey8.join();
+               monkey9.join();
+               monkey10.join();
+           }catch(InterruptedException e){
+               e.printStackTrace();
+               System.out.println("Safe Monkeys Interrupted");
+           }
 
 
-
-
-        // This wait is here because main is still a thread and we want the main method to print the finished copies
-        // after enough time has passed.
+           // This wait is here because main is still a thread and we want the main method to print the finished copies
+           // after enough time has passed.
         try {
             Thread.sleep(1000);
         } catch(InterruptedException e) {
@@ -57,5 +83,7 @@ public class MonkeyTypewriter {
         }
 
         // Print out the copied versions here.
+        System.out.println("\nUnsafe:\n" + unsafe1.copied);
+        System.out.println("\nSafe:\n" + safe1.copied);
     }
 }
